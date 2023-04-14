07cb03f1ca922fb4d7ec3ee22f8bd46dab02efb0
Viola Campos <viola.campos@hs-rm.de>
2023-04-14 11:40:27 +0200

    Add notebook to select local fixes

--- Code/create_CVEfixes_from_dump.sh
+++ Code/create_CVEfixes_from_dump.sh
@@ -3,7 +3,7 @@
 # create a SQLite3 database file of the CVEfixes.
 
 # ------------------------------------------------------------------------------
-DATA_PATH=Output/
-gzcat $DATA_PATH/CVEfixes.sql.gz | sqlite3 $DATA_PATH/CVEfixes.db
+DATA_PATH=Data
+zcat $DATA_PATH/CVEfixes_v1.0.7.sql.gz | sqlite3 $DATA_PATH/CVEfixes.db
 
 #------------------------------------------------------------------------------
